94fb8afcdd97ab45cb36c10443ee301c3d7332ed
Robinson Chalas <robinsonchalasj@gmail.com>
2024-05-23 20:59:18 -0700

    Section 1 and 2 are responsive

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.nav-list');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}; 
+
+

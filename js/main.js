60cbab29eba4474da0f7611bbdc563d0833aba25
Robinson Chalas <robinsonchalasj@gmail.com>
2024-05-25 13:14:02 -0700

    only miss the footer

--- js/main.js
+++ js/main.js
@@ -7,3 +7,25 @@ menu.onclick = () => {
 }; 
 
 
+
+
+// Body dark and Light
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+    }
+
+    toggleButton.addEventListener('click', function () {
+        document.body.classList.toggle('dark-theme');
+
+        let theme = 'light';
+        if (document.body.classList.contains('dark-theme')) {
+            theme = 'dark';
+        }
+        localStorage.setItem('theme', theme);
+    });
+});
